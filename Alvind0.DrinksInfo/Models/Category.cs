4e41b58a2517fca6a7e86d883ec9dd7d8e68849d
Alvind0 <170922866+Alvind0@users.noreply.github.com>
2025-01-30 22:05:43 +0900

    feat: APIのデータ構造に合わせたDrinkとCategoryのモデルを新規作成

---
+++ Alvind0.DrinksInfo/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+namespace Alvind0.DrinksInfo.Models;
+
+public class Category
+{
+    [JsonPropertyName("strCategory")]
+    public required string CategoryName { get; set; }
+}
