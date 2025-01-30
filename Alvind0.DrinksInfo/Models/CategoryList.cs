591c8e3e9c4239bfef8b2f2c681542f4f9d992bf
Alvind0 <170922866+Alvind0@users.noreply.github.com>
2025-01-30 23:05:30 +0900

    fix: Jsonの構造上、直接リストにデシリアライズすることができないためCategoryとDrinkのラッパークラスを作成

---
+++ Alvind0.DrinksInfo/Models/CategoryList.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Alvind0.DrinksInfo.Models;
+
+internal class CategoryList
+{
+    [JsonPropertyName("drinks")]
+    public required List<Category> Categories { get; set; }
+}
---
+++ Alvind0.DrinksInfo/Models/DrinkList.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Alvind0.DrinksInfo.Models;
+
+public class DrinkList
+{
+    [JsonPropertyName("drinks")]
+    public List<Drink> Drinks { get; set; }
+}
